5486a9bd5e27a835dabb20a5c416a5c8e766ec80
Israfiel <174603598+israfiel-a@users.noreply.github.com>
2024-10-05 14:51:38 -0400

    forgot to delete the old stuff oops

--- src/external/models/rendering.h
+++
@@ -1,8 +0,0 @@
-#ifndef LETO__MODELS__RENDERING
-#define LETO__MODELS__RENDERING
-
-#include "structure.h"
-
-void LetoRenderModel(leto_model_t *model, unsigned int shader);
-
-#endif // LETO__MODELS__RENDERING
